280b10711261b17a6b1bf8a73879452292bcffd4
Mathis Van Eetvelde <mathis.vaneetvelde@gmail.com>
2017-02-06 14:35:50 +0100

    Added LiquidCrystal + Fixes

--- Arduino_Clock/Arduino_Clock/Arduino_Clock.ino
+++ Arduino_Clock/Arduino_Clock/Arduino_Clock.ino
@@ -2,6 +2,8 @@
 Mathis Van Eetvelde 2/6/17
 */
 
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
 
 int seconds = 0;
@@ -12,7 +14,8 @@ int clockOn = 1; //I had to disable the clock to test some things
 void setup() {
 Serial.begin(9600);
 Serial.print("Starting to count!");
-
+lcd.begin(16, 2);
+lcd.setCursor(0,0);
 
 }
 
@@ -20,6 +23,9 @@ void loop() {
 
 if(clockOn = 1){
     seconds ++;
+    checkTime();
+    printTime();
+    delayMicroseconds(1000);
 } else {
 
     Serial.print("The clock is disabled, please change clockOn int to 1!");
@@ -42,4 +48,14 @@ void checkTime(){
         minutes = 0;
         Serial.print("An hour has gone by!");
     }
+}
+void printTime(){
+    lcd.setCursor(0,1);
+    lcd.print(hours);
+    lcd.setCursor(2,1);
+    lcd.print(":");
+    lcd.setCursor(4,1);
+    lcd.print(minutes);
+    lcd.setCursor(6,1);
+    lcd.print(seconds);
 }
