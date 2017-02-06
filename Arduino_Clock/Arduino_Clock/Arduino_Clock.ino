afbe4a137465b5cd9bc7c8bf28846ebeede57748
Mathis Van Eetvelde <mathis.vaneetvelde@gmail.com>
2017-02-06 15:05:10 +0100

    Minor Fixes

--- Arduino_Clock/Arduino_Clock/Arduino_Clock.ino
+++ Arduino_Clock/Arduino_Clock/Arduino_Clock.ino
@@ -15,7 +15,7 @@ void setup() {
 Serial.begin(9600);
 Serial.print("Starting to count!");
 lcd.begin(16, 2);
-lcd.setCursor(0,0);
+
 
 }
 
@@ -25,7 +25,9 @@ if(clockOn = 1){
     seconds ++;
     checkTime();
     printTime();
-    delayMicroseconds(1000);
+    delay(1000);
+    lcd.clear();
+    preTime();
 } else {
 
     Serial.print("The clock is disabled, please change clockOn int to 1!");
@@ -57,5 +59,12 @@ void printTime(){
     lcd.setCursor(4,1);
     lcd.print(minutes);
     lcd.setCursor(6,1);
+    lcd.print(":");
+    lcd.setCursor(8,1);
     lcd.print(seconds);
 }
+void preTime(){
+  lcd.setCursor(0,0);
+  lcd.print("The Time Is:");
+  
+  }
